0b2468d73e1ca3d05d92820272fc3d07ca484f3b
Taisei Omine <ominetaisei@gmail.com>
2022-07-12 19:55:03 +0900

    test

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+import axios from 'axios';
+import { wrapper } from 'axios-cookiejar-support';
+import { CookieJar } from 'tough-cookie';
+
+const jar = new CookieJar();
+const client = wrapper(axios.create({ jar }));
+
+const { config } = await client.get('https://httpbin.org/cookies/set/session/userid');
+
+console.log(config.jar.toJSON());
+
+return 
+
 const core = require('@actions/core');
 // const github = require('@actions/github');
 const axios = require('axios');
